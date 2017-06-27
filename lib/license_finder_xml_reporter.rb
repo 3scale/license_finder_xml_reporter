5d69f286e5b59ae8fc5b1aeade104de8fb86ae0e
dd di cesare <didi@posteo.net>
2017-06-27 15:46:16 +0200

    [license finder] reopen their main instead of creating ours

--- lib/license_finder_xml_reporter.rb
+++ lib/license_finder_xml_reporter.rb
@@ -1,5 +1,5 @@
-require "license_finder_xml_reporter/version"
 require 'license_finder'
+require "license_finder_xml_reporter/version"
 
 module LicenseFinderXmlReporter
   # Your code goes here...
--- lib/license_finder_xml_reporter/cli/main.rb
+++ lib/license_finder_xml_reporter/cli/main.rb
@@ -1,10 +1,10 @@
-require 'license_finder/cli/main'
+require 'license_finder'
 require 'license_finder_xml_reporter/reports/xml_report.rb'
 
 module LicenseFinder
   module CLI
-    class ThreescaleMain < Main
-      FORMATS = Main::FORMATS.merge!({'xml' => XmlReport })
+    class Main
+      FORMATS = Main::FORMATS.merge!({'xml' => LicenseFinderXmlReporter::XmlReport })
       class_option :format, desc: "The desired output format.", default: 'text', enum: FORMATS.keys
     end
   end
